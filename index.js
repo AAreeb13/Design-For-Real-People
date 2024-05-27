04a685ff2bfcf73094d227bec19157f7799acf08
Basit Khan <bmk22@aube.doc.ic.ac.uk>
2024-05-27 13:22:50 +0100

    Add example tshirt API

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const app = require("express")();
 const PORT = 8080;
 
+app.get("/tshirt", (req, res) => {
+  res.status(200).send({
+    tshirt: "T",
+    size: "large"
+  })
+});
+
 app.listen(
   PORT,
   () => console.log(`It's alive on http://localhost:${PORT}`)
