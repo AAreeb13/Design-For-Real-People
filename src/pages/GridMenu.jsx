a8a2d66c3ecc9ecb644662209cb8bf423ef98053
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-06-03 21:05:50 +0100

    Implemented frontend for grid menu

--- src/pages/GridMenu.jsx
+++ src/pages/GridMenu.jsx
@@ -1,6 +1,51 @@
+import React from 'react';
 
-const GridMenu = () => {
-    return <h1>Hello world 2</h1>
+const RoadmapButtons = () => {
+    const mathTopics = ['Calculus', 'Probability and Statistics', 'Analysis', 'Linear Algebra'];
+    const languages = ['Python', 'Java', 'C', 'C++', 'Haskell'];
+        
+    const gridButton = {
+        width: "20%",
+        height: "70px",
+        marginBottom: "10px",
+        fontSize: "18px",
+        fontWeight: "bold",
+        borderRadius: "8px",
+        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+        transition: "transform 0.2s",
+        margin: "1% 5%"
+    };
+
+    const gridContainer = {
+        maxWidth: "100%",
+        margin: "0 auto",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+    };
+
+    const gridSection = {
+        width: "100%",
+        marginBottom: "30px",
+    };
+
+    return (
+        <div className="col" style={gridContainer}>
+            <div style={gridSection}>
+                <h2 style={{ marginBottom: "20px" }}>Mathematics</h2>
+                {mathTopics.map((language, index) => (
+                    <button key={index} className="btn btn-light btn-outline-success" style={gridButton}>{language}</button>
+                ))}
+            </div>
+
+            <div style={gridSection}>
+                <h2 style={{ marginBottom: "20px" }}>Programming</h2>
+                {languages.map((profession, index) => (
+                    <button key={index} className="btn btn-light btn-outline-success" style={gridButton}>{profession}</button>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default GridMenu
+export default RoadmapButtons;
