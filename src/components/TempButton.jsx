8796e40e5809051241cc90bee14d46d77fcb0466
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-06-04 09:49:11 +0100

    Removed redundant files

--- src/components/TempButton.jsx
+++
@@ -1,8 +0,0 @@
-import { getGraphData } from "./../../database/graphData"
-
-function TempButton() {
-    return <button onClick={getGraphData}>Click Here to fetch data</button>
-}
-
-
-export default TempButton;
