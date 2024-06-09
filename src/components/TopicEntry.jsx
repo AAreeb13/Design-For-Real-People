115ca73536d6c657ad3f9b063182f532f4c37775
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-06-09 16:08:37 +0100

    implemented frontend for topic display

--- src/components/TopicEntry.jsx
+++ src/components/TopicEntry.jsx
@@ -4,6 +4,7 @@ import { getGraphData } from "../../database/graphData";
 const TopicEntry = ({ node }) => {
   const [topicNode, setTopicNode] = useState(null);
   const [error, setError] = useState(null);
+  const [completed, setCompleted] = useState(false); // State to track completion status
 
   useEffect(() => {
     const fetchTopic = async () => {
@@ -22,6 +23,10 @@ const TopicEntry = ({ node }) => {
     fetchTopic();
   }, [node]);
 
+  const toggleCompletion = () => {
+    setCompleted(!completed);
+  };
+
   if (error) {
     return <div>{error}</div>;
   }
@@ -32,32 +37,46 @@ const TopicEntry = ({ node }) => {
 
   return (
     <div>
-      <h1 className="topic-header">{topicNode.subject}</h1>
-      <h2 className="topic-name">{topicNode.name}</h2>
-
-      <h3>Description:</h3>
-      <p className="topic-description">{topicNode.description}</p>
-
-      <h3>Learning Objectives:</h3>
-      <ul>
-        {topicNode.learning_objectives.map((o, index) => (
-          <li key={index}>{o}</li>
-        ))}
-      </ul>
-
-      <h3>Prerequisites:</h3>
-      <ul>
-        {topicNode.requires.map((o, index) => (
-          <li key={index}>{o}</li>
-        ))}
-      </ul>
-      
-      <h3>Resources:</h3>
-      <ul>
-        {topicNode.resources.map((o, index) => (
-          <li key={index}>{o}</li>
-        ))}
-      </ul>
+      <button
+        style={{
+          position: "fixed",
+          top: "10px",
+          right: "10px",
+          zIndex: "9999",
+          marginTop: "80px"
+        }}
+        onClick={toggleCompletion}
+      >
+        {completed ? "Completed ✔️" : "Mark as Completed"}
+      </button>
+      <div>
+        <h1 className="topic-header">{topicNode.subject}</h1>
+        <h2 className="topic-name">{topicNode.name}</h2>
+  
+        <h3>Description:</h3>
+        <p className="topic-description">{topicNode.description}</p>
+  
+        <h3>Learning Objectives:</h3>
+        <ul>
+          {topicNode.learning_objectives.map((o, index) => (
+            <li key={index}>{o}</li>
+          ))}
+        </ul>
+  
+        <h3>Prerequisites:</h3>
+        <ul>
+          {topicNode.requires.map((o, index) => (
+            <li key={index}>{o}</li>
+          ))}
+        </ul>
+        
+        <h3>Resources:</h3>
+        <ul>
+          {topicNode.resources.map((o, index) => (
+            <li key={index}>{o}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
