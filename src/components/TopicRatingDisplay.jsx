38aa38caaa4495fcae91bebb857ebba78f646eaf
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-06-12 09:07:32 +0100

    feat: implemented frontend for edit and delete buttons

--- src/components/TopicRatingDisplay.jsx
+++ src/components/TopicRatingDisplay.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { BsEmojiSmile, BsEmojiNeutral, BsEmojiFrown } from "react-icons/bs";
-import { getRating } from "../../database/firebase";
+import { RiDeleteBin6Line, RiEdit2Line } from "react-icons/ri";
 import { getNode } from "../../database/graphData";
+import '../styles/TopicRatingDisplay.css'; 
 
 const TopicRatingDisplay = ({ topicName }) => {
   const [topicNode, setTopicNode] = useState(null);
@@ -19,24 +20,48 @@ const TopicRatingDisplay = ({ topicName }) => {
     return () => clearInterval(interval);
   }, [topicName]);
 
+  const handleDelete = () => {
+    console.log("Delete button clicked for topic:", topicName);
+
+  };
+
+  const handleEdit = () => {
+    console.log("Edit button clicked for topic:", topicName);
+
+  };
+
   if (!topicNode) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="rating-icons" style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
-      <div style={{ color: "green" }}>
-        <BsEmojiSmile size={50} />
-        <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.good.low}</span>
+    <div className="rating-icons-container">
+      <button className="btn btn-outline-danger" onClick={handleDelete}>
+        <RiDeleteBin6Line size={30} className="icon" />
+        <span className="button-text">Delete</span>
+      </button>
+
+      <div className="rating-icons" style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px" }}>
+        <div style={{ color: "green" }}>
+          <BsEmojiSmile size={50} />
+          <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.good.low}</span>
+        </div>
+
+        <div style={{ color: "yellow" }}>
+          <BsEmojiNeutral size={50} />
+          <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.alright.low}</span>
+        </div>
+
+        <div style={{ color: "red" }}>
+          <BsEmojiFrown size={50} />
+          <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.bad.low}</span>
+        </div>
       </div>
-      <div style={{ color: "yellow"}}>
-        <BsEmojiNeutral size={50} />
-        <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.alright.low}</span>
-      </div>
-      <div style={{ color: "red" }}>
-        <BsEmojiFrown size={50} />
-        <span style={{ display: "block", textAlign: "center", color: "black" }}>{topicNode.bad.low}</span>
-      </div>
+
+      <button className="btn btn-outline-secondary edit-button" onClick={handleEdit}>
+        <RiEdit2Line size={30} className="icon" />
+        <span className="button-text">Edit</span>
+      </button>
     </div>
   );
 };
