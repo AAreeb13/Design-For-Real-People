cae14522a863eb9c11946e631331502868a9ffd7
RayanAkhtar <113346804+RayanAkhtar@users.noreply.github.com>
2024-06-13 08:43:19 +0100

    feat: User suggestions in a topic are now shown to the moderator in real time

--- src/pages/TopicEditForm.jsx
+++ src/pages/TopicEditForm.jsx
@@ -17,7 +17,7 @@ const TopicEditForm = ({ topicName }) => {
         const fetchedFormData = await getFormData(topicName);
         setTopicNode(fetchedTopicNode);
         console.log("fetchedNode", fetchedTopicNode)
-        setMessages(fetchedTopicNode.suggestions.map((d, i) => {return {text: d, id: i}}))
+        setMessages(fetchedTopicNode.suggestions.map((d, i) => { return { text: d, id: i } }));
         setFormData(fetchedFormData);
       } catch (error) {
         console.error("Error fetching topic data:", error);
@@ -25,6 +25,10 @@ const TopicEditForm = ({ topicName }) => {
     };
 
     fetchTopicData();
+
+    const interval = setInterval(fetchTopicData, 3000);
+
+    return () => clearInterval(interval);
   }, [topicName]);
 
   const handleInputChange = (e, label, isTopicNode) => {
@@ -89,20 +93,19 @@ const TopicEditForm = ({ topicName }) => {
   const handleDeleteMessage = async (id) => {
     try {
       const messageToDelete = messages.find(message => message.id === id);
-  
+
       if (!messageToDelete) {
         console.error(`Message with id ${id} not found.`);
         return;
       }
       await deleteSuggestionFromTopic(topicName, messageToDelete.text);
-  
+
       setMessages(prevMessages => prevMessages.filter(message => message.id !== id));
     } catch (error) {
       console.error("Error deleting message:", error);
       alert("An error occurred while deleting the message. Please try again later.");
     }
   };
-  
 
   const renderFormElement = (label, value, isTopicNode) => {
     const topicNodePropsToUse = ["name", "subject", "description", "learning_objectives"];
@@ -110,7 +113,6 @@ const TopicEditForm = ({ topicName }) => {
     if (isTopicNode) {
       if (topicNodePropsToUse.includes(label)) {
         let processedValue = value;
-
 
         if (Array.isArray(value)) {
           processedValue = value.join('\n');
@@ -130,7 +132,6 @@ const TopicEditForm = ({ topicName }) => {
         );
       }
     } else {
-
       let processedValue = value;
       if (Array.isArray(value)) {
         processedValue = value.join(`\n`);
@@ -176,7 +177,7 @@ const TopicEditForm = ({ topicName }) => {
         {showAside && (
           <aside className="message-aside">
             <div className="d-flex justify-content-end">
-              <h2 style={{marginRight: "auto"}}>Suggestions</h2>
+              <h2 style={{ marginRight: "auto" }}>Suggestions</h2>
               <Button variant="info" onClick={() => setShowAside(false)}>Close</Button>
             </div>
             {messages.map(message => (
@@ -199,4 +200,3 @@ const asTitle = (str) => {
 }
 
 export default TopicEditForm;
-
